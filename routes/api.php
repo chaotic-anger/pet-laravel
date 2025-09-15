57a473cd54aa07a7c28b77cfa9bd67bdda301543
n-boykov <nvb@ugraweb.ru>
2025-09-15 08:10:41 +0500

    https://github.com/chaotic-anger/pet-laravel/issues/8 проба пера

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\Api\Api\CommentController;
 use App\Http\Controllers\Api\Api\PostController;
 
-Route::apiResource('posts', PostController::class);
-Route::prefix('posts/{post}')->group(function () {
-    Route::apiResource('comments', CommentController::class)->shallow();
-    Route::post('comments/{comment}/vote/{direction}', [CommentController::class, 'vote'])
-        ->where('direction', 'up|down');
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::apiResource('posts', PostController::class);
+    Route::prefix('posts/{post}')->group(function () {
+        Route::apiResource('comments', CommentController::class)->shallow();
+        Route::post('comments/{comment}/vote/{direction}', [CommentController::class, 'vote'])
+            ->where('direction', 'up|down');
+    });
 });
+
